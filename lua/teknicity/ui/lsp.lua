6245960c3fc7e11965381121385f3296db86e774
rustx <rustx@teknicity.net>
2023-03-25 23:43:13 +0100

    chore(lsp) add language servers

--- lua/teknicity/ui/lsp.lua
+++ lua/teknicity/ui/lsp.lua
@@ -10,7 +10,6 @@ lsp.ensure_installed({
   'gopls',
   'volar',
   'emmet_ls',
-  'eslint',
   'html',
   'cssls',
   'tsserver',
@@ -19,21 +18,23 @@ lsp.ensure_installed({
   'prismals',
   'svelte',
   'tflint',
+  'terraformls',
+  'yamlls'
 })
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
     }
+  }
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<Up>'] = cmp.mapping.select_prev_item(cmp_select),
@@ -49,17 +50,17 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -73,9 +74,18 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+lsp.format_mapping('gq', {
+  servers = {
+    ['lua_ls'] = { 'lua' },
+    ['rust_analyzer'] = { 'rust' },
+    ['yamlls'] = { 'yaml' },
+    ['gopls'] = { 'go' },
+    ['tsserver'] = { 'typescript' },
+  }
+})
+
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
-
