eea7eebc66abe74ad86864c3005af04a23ab954d
rustx <rustx@teknicity.net>
2024-01-30 09:45:06 +0100

    chore(config) update telescope and lsp

--- lua/teknicity/ui/lsp.lua
+++ lua/teknicity/ui/lsp.lua
@@ -63,6 +63,7 @@ lsp.configure('helm_ls', {
 })
 
 lsp.configure('yamlls', {
+  filetypes = { "yaml", "yaml.docker-compose" },
   settings = {
     yaml = {
       keyOrdering = false
@@ -116,6 +117,9 @@ lsp.on_attach(function(_, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+  vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+  vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vwa", function() vim.lsp.buf.add_workspace_folder() end, opts)
